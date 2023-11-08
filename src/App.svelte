c4ff2f6bade0fd5a80870a821c6fd0a1e13135cf
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-07 23:15:39 -0500

    Screen reader compatibility test

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   let fontSize = 10;
 </script>
-<button on:click={e => fontSize++}>+</button>
-<button on:click={e => fontSize--}>-</button>
-<p style="font-size: {1 + fontSize + 'px'}">Hello.</p>
+<button on:click={e => fontSize++} id="foo">+</button>
+<button on:click={e => fontSize--} id="bar">-</button>
+<p style="font-size: {1 + fontSize + 'px'}">Hello!</p>
