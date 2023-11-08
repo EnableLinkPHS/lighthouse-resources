c5586bc003fdec132e8fc7d9b4f327e8c80489aa
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-07 23:17:06 -0500

    aria-label compatiblity test

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,6 @@
 <script>
   let fontSize = 10;
 </script>
-<button on:click={e => fontSize++} id="foo">+</button>
-<button on:click={e => fontSize--} id="bar">-</button>
+<button on:click={e => fontSize++} aria-label="foo">+</button>
+<button on:click={e => fontSize--} aria-label="bar">-</button>
 <p style="font-size: {1 + fontSize + 'px'}">Hello!</p>
