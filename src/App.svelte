a8523a45b64e2c8a4dbfd8021921f248d2e0c74d
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-07 23:08:47 -0500

    Font size change test

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,6 @@
-<p>hello</p>
+<script>
+  let fontSize = 10;
+</script>
+<button on:click={e => fontSize++}>+</button>
+<button on:click={e => fontSize--}>-</button>
+<p style="font-size: {1 + fontSize + 'px'}">Hello.</p>
