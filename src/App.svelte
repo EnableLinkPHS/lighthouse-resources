4bc92ad9236a050bc32e2dfacae785fb304a2fd6
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-07 23:17:20 -0500

    .

--- src/App.svelte
+++ src/App.svelte
@@ -3,4 +3,4 @@
 </script>
 <button on:click={e => fontSize++} aria-label="foo">+</button>
 <button on:click={e => fontSize--} aria-label="bar">-</button>
-<p style="font-size: {1 + fontSize + 'px'}">Hello!</p>
+<p style="font-size: {1 + fontSize + 'px'}">Hello.</p>
