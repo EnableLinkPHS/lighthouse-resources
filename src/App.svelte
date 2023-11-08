f445364d1061a5f34c235a7bf88a78a5c6d2fbda
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-07 23:36:55 -0500

    .

--- src/App.svelte
+++ src/App.svelte
@@ -3,4 +3,4 @@
 </script>
 <button on:click={e => fontSize++} label="Increase font size">+</button>
 <button on:click={e => fontSize = Math.max(0, fontSize - 1)} label="Decrease font size">-</button>
-<p style="font-size: {10 + fontSize + 'px'}">Hello!</p>
+<p style="font-size: {10 + fontSize + 'px'}">Hello.</p>
