5c469f2f117cef834d3b088530d6ec92c9e43b54
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-12 12:37:13 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,5 +2,5 @@
   let fontSize = 0;
 </script>
 <button on:click={e => fontSize = Math.min(60, fontSize + 1)} aria-label="Increase font size">+</button>
-<button on:click={e => fontSize = Math.max(1, fontSize - 1)} aria-label="Decrease font size">-</button>
+<button on:click={e => fontSize = Math.max(-3, fontSize - 1)} aria-label="Decrease font size">-</button>
 <p style="font-size: {10 + fontSize + 'px'}">Hello.</p>
