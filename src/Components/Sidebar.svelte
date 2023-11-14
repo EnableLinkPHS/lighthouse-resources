1be4107ee3249d2487fcca34a93ceeb7cf1c9a85
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-14 19:23:24 +0000

    Add folders and Sidebar file

---
+++ src/Components/Sidebar.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
+</script>
+
+<Sidebar>
+  <SidebarWrapper>
+    <SidebarGroup>
+      <SidebarItem label = "A"></SidebarItem>
+			<SidebarItem label = "B"></SidebarItem>
+    </SidebarGroup>
+  </SidebarWrapper>
+</Sidebar>
