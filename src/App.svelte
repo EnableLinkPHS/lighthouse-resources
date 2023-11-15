62b9e69984af79621a9fcee581441410eff79caa
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-15 14:10:12 -0500

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <script>
   let fontSize = 0;
+  import { Sidebar } from "./Sidebar.svelte";
 </script>
 <button on:click={e => fontSize = Math.min(60, fontSize + 1)} aria-label="Increase font size">+</button>
 <button on:click={e => fontSize = Math.max(-3, fontSize - 1)} aria-label="Decrease font size">-</button>
 <p style="font-size: {10 + fontSize + 'px'}">Hello.</p>
+<Sidebar/>
