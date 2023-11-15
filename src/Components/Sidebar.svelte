e95f805abfb47ae2fa7b7f12f0c6567ee4dea6e9
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-15 14:14:57 -0500

    Sidebar test

--- src/Components/Sidebar.svelte
+++ src/Components/Sidebar.svelte
@@ -1,12 +1,28 @@
-<script>
-  import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper } from 'flowbite-svelte';
-</script>
+<div class="Sidebar">
+  <p>A</p> 
+  <p>B</p>
+  <p>C</p>
+</div>
 
-<Sidebar>
-  <SidebarWrapper>
-    <SidebarGroup>
-      <SidebarItem label = "A"></SidebarItem>
-      <SidebarItem label = "B"></SidebarItem>
-    </SidebarGroup>
-  </SidebarWrapper>
-</Sidebar>
+<style>
+.sidebar {
+    position: fixed;
+    top: 138px;
+    width: fit-content;
+    height: 100%;
+    padding: 10px;
+    background: rgb(92, 91, 91); /* 231F20 */
+}
+
+.sidebar p {
+    padding: 2px;
+    width: 180px;
+    border: 2px solid white;
+    text-align: center;
+    margin: 0px auto 10px auto; /* top right bottom left*/
+    font-size: 20px;
+    background: black;
+    color: white;
+    overflow: auto;
+}
+</style>
