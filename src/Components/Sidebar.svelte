b7e485e24c40bf5da5b92c1eb5f8c79ecbb32d87
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-11-16 19:29:45 +0000

    Update Sidebar

--- src/Components/Sidebar.svelte
+++ src/Components/Sidebar.svelte
@@ -1,28 +1,37 @@
 <div class="Sidebar">
-  <p>A</p> 
-  <p>B</p>
-  <p>C</p>
+  <p>Supply Stores</p> 
+  <p>Vocational Rehab</p>
+  <p>Transporation</p>
+	<p>MD DORS</p>
+	<p>Deafblind Services</p>
+	<p>Housing</p>
+	<p>Mobile Apps</p>
 </div>
-
+<link href='https://fonts.googleapis.com/css?family=Roboto Slab' rel='stylesheet'>
 <style>
-.Sidebar {
-    position: fixed;
-    top: 138px;
-    width: fit-content;
-    height: 100%;
-    padding: 10px;
-    background: rgb(92, 91, 91); /* 231F20 */
-}
+	.Sidebar {
+	    position: fixed;
+	    top: 20%;
+	    width: 20%;
+	    height: 100%;
+	    padding: 5px;
+	    background: #2b175f
+	}
+	
+	.Sidebar p {
+			font-family: 'Roboto Slab';
+	    padding: 6px;
+		  padding-top: 5%;
+			padding-bottom: 5%;
+	    border-bottom: 2px solid white;
+	    text-align: left;
+	    margin: 0px;
+	    font-size: 15px;
+	    color: white;
+	    overflow: auto;
+	}
 
-.Sidebar p {
-    padding: 2px;
-    width: 180px;
-    border: 2px solid white;
-    text-align: center;
-    margin: 0px auto 10px auto; /* top right bottom left*/
-    font-size: 20px;
-    background: black;
-    color: white;
-    overflow: auto;
-}
+	.Sidebar p:last-child {
+		border-bottom: 0px;
+	}
 </style>
