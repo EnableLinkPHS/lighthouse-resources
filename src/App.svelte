78c734ce5198504cd7b6eac1dd2558f8af437392
Kartavya Gaur <kartavya.gaur@gmail.com>
2023-12-04 18:18:09 -0500

    sayan

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
   let fontSize = 0;
   import Sidebar from "./Components/Sidebar.svelte";
+  import Header from "./Components/Header.svelte";
 </script>
 <button on:click={e => fontSize = Math.min(60, fontSize + 1)} aria-label="Increase font size">+</button>
 <button on:click={e => fontSize = Math.max(-3, fontSize - 1)} aria-label="Decrease font size">-</button>
 <p style="font-size: {10 + fontSize + 'px'}">Hello.</p>
 <Sidebar/>
+
+<Header/>
---
+++ src/Components/Header.svelte
@@ -0,0 +1,8 @@
+<nav id = "header">
+    {#each navitems as item}<p>{item}</p>{/each}
+</nav>
+
+
+<script>
+    let  navitems = ["Resize Text", "High Contrast", "Font Style", "Link Underline", "<temp>"];
+</script>
