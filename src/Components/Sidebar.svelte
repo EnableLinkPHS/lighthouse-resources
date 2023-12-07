8881495a83a9d38c65352caf391159e4570d227d
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-12-07 05:07:12 +0000

    Attempt at JSON functionality

--- src/Components/Sidebar.svelte
+++ src/Components/Sidebar.svelte
@@ -1,44 +1,36 @@
-<nav class="Sidebar">
-	{#each sideitems as [item, url]}
-		<p><a href={url}>{item}</a></p>
+<script context="module">
+	import data from '../data.json';
+</script>
+<aside class="Sidebar">
+	{#each data.sideitems as item}
+		<p><a href=#top>{item}</a></p>
 	{/each}
-</nav>
+</aside>
 <link href='https://fonts.googleapis.com/css?family=Roboto Slab' rel='stylesheet'>
 <style>
 	.Sidebar {
-		position: fixed;
-		top: 20%;
-		width: 20%;
-		height: 100%;
-		padding: 5px;
-		background: #2b175f
+	    position: fixed;
+	    top: 20%;
+	    width: 20%;
+	    height: 100%;
+	    padding: 5px;
+	    background: #2b175f
 	}
 	
 	.Sidebar p {
-		font-family: 'Roboto Slab';
-	    	padding: 6px;
-		padding-top: 5%;
-		padding-bottom: 5%;
-		border-bottom: 2px solid white;
-		text-align: left;
-		margin: 0px;
-		font-size: 15px;
-		color: white;
-		overflow: auto;
+			font-family: 'Roboto Slab';
+	    padding: 6px;
+		  padding-top: 5%;
+			padding-bottom: 5%;
+	    border-bottom: 2px solid white;
+	    text-align: left;
+	    margin: 0px;
+	    font-size: 15px;
+	    color: white;
+	    overflow: auto;
 	}
 
 	.Sidebar p:last-child {
 		border-bottom: 0px;
 	}
 </style>
-<script>
-	let sideitems = [
-		["Supply Stores", "#stores"],
-		["Vocational Rehab", "#rehab"],
-		["Transportation", "#transport"],
-		["MD DORS", "#mddors"],
-		["DeafBlind Services", "#dbs"],
-		["Housing", "#housing"],
-		["Mobile Apps", "#apps"]
-	];
-</script>
