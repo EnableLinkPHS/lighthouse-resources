6ed9768e3503913f4d810054e3f637b537dbaf44
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-12-18 18:50:14 +0000

    Header update

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -1,7 +1,27 @@
-<aside id = "header">
-    {#each navitems as item}<p>{item}</p>{/each}
+<aside class = "header">
+    <button><i></i>Increase Font</button>
+	<button><i></i>Decrease Font</button>
+	<button><i></i>Grayscale</button>
+	<button><i></i>High Contrast</button>
+	<button><i></i>URL Underline</button>
 </aside>
-
+<style>
+    .Header button {
+		padding: 1%;
+		width: 15%;
+		float: left;
+		background-color: #2b175f;
+		border: none;
+		font-family: 'Roboto Slab';
+		font-size: 15px;
+		text-align: center;
+		display: block;
+	}
+	.Header button:hover {
+		background-color: #eeeeee;
+		color: #000000;
+	}
+</style>
 
 <script>
     let navitems = [
