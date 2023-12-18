752acc2df457a922d52f0974fa6711ddcc864d3f
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2023-12-18 19:06:58 +0000

    Yes.

--- src/Components/Header.svelte
+++ src/Components/Header.svelte
@@ -1,14 +1,3 @@
 <aside id = "header">
-    {#each navitems as item}<p>{item}</p>{/each}
+    
 </aside>
-
-
-<script>
-    let navitems = [
-        "Resize Text", 
-        "High Contrast", 
-        "Font Style", 
-        "Link Underline", 
-        "<temp>"
-    ];
-</script>
