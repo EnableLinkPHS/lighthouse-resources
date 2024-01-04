3f35a52b4d3c3b04c996ab6741843717ab31d0ee
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2024-01-04 19:00:48 +0000

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,8 @@
   import Sidebar from "./Components/Sidebar.svelte";
   import Header from "./Components/Header.svelte";
 </script>
-<button on:click={e => fontSize = Math.min(60, fontSize + 1)} aria-label="Increase font size">+</button>
-<button on:click={e => fontSize = Math.max(-3, fontSize - 1)} aria-label="Decrease font size">-</button>
+<!--button on:click={e => fontSize = Math.min(60, fontSize + 1)} aria-label="Increase font size">+</button>
+<button on:click={e => fontSize = Math.max(-3, fontSize - 1)} aria-label="Decrease font size">-</button-->
 <p style="font-size: {10 + fontSize + 'px'}">Hello.</p>
 <Sidebar/>
 
