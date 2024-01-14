ba5d400ce245bc0af26120e2eea2ab4b407ae356
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2024-01-14 18:33:26 +0000

    Fix tabindex

--- src/Components/Page.svelte
+++ src/Components/Page.svelte
@@ -2,21 +2,26 @@
     import data from "../Assets/data.json";
     export let current = "home";
 </script>
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 <main class="Page">
-    <p>{data[current].flavor}</p>
-    {#if data[current].thead}
-        <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+    <p tabindex="0">{data[current].flavor}</p>
+    {#if data[current].thead} 
         <table tabindex="0">
-            <tr>{#each data[current].thead as header}
-                <th>{header}</th>
+            <tr>
+                {#each data[current].thead as header}
+                    <th tabindex="0">{header}</th>
                 {/each}
             </tr>
             {#each data[current].table as row}
-                <tr>{#each Object.values(row) as cell}<td>{cell}</td>{/each}</tr>
+                <tr>
+                    {#each Object.values(row) as cell}
+                        <td tabindex="0">{cell}</td>
+                    {/each}
+                </tr>
             {/each}
         </table>
     {:else}
-        <p>Welcome to the site!</p>
+        <p tabindex="0">Welcome to the site!</p>
     {/if}
 </main>
 <style>
