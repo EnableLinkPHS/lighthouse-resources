51e16703ebe3a6475c5fdc2fbb78611e9da4d234
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2024-01-19 01:48:58 +0000

    Smooth conflicting commits

--- src/App.svelte
+++ src/App.svelte
@@ -5,14 +5,10 @@
   import { pageContrast} from "./store.js";
   let page = "home";
 </script>
-<!--button on:click={e => fontSize = Math.min(60, fontSize + 1)} aria-label="Increase font size">+</button>
-<button on:click={e => fontSize = Math.max(-3, fontSize - 1)} aria-label="Decrease font size">-</button>
-<p style="font-size: {10 + fontSize + 'px'}">Hello.</p-->
 <a id="skip-link" href="#page">Skip to main content</a>
 <Sidebar bind:page contrast={$pageContrast}/>
 <Header/>
 <Page current={page}/>
-<p>Page: {page}; Contrast: {contrast}; Font Size: {fontSize}; Underline: {underline}</p>
 <style>
   #skip-link {
     position: absolute;
