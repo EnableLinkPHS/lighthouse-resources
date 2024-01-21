e4013e1c6085e56ede5da84fad7fb0dcce2718c9
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2024-01-21 17:53:39 +0000

    Adjust default size

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
 import { writable } from "svelte/store";
 export const pageContrast = writable("none");
-export const fontSize = writable(10);
+export const fontSize = writable(16);
 export const underline = writable(false);
