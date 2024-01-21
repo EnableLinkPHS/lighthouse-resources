f5095ece4dd84912c807130ea4ba4d01dca47f6b
SayanC4 <103901872+SayanC4@users.noreply.github.com>
2024-01-21 17:56:46 +0000

    Comment out testing <p>

--- src/Components/Page.svelte
+++ src/Components/Page.svelte
@@ -8,7 +8,7 @@
 </script>
 <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
 <main class="Page" id="page" style="--linkUnderline: {linkUnderline}; --textSize: {textSize}">
-    <p tabindex="0">Contrast: {$pageContrast}, Size: {textSize}, Underline: {linkUnderline}</p>
+    <!--p tabindex="0">Contrast: {$pageContrast}, Size: {textSize}, Underline: {linkUnderline}</p-->
     <p tabindex="0">{data[current].flavor}</p>
     {#if data[current].thead} 
         <table tabindex="0">
